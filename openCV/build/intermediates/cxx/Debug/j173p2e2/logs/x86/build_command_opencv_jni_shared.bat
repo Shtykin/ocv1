ede74e30723724eed913622e090e2157c879d278
Shtykin <101437346+Shtykin@users.noreply.github.com>
2024-01-25 10:47:38 +0800

    init commit

---
+++ openCV/build/intermediates/cxx/Debug/j173p2e2/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\User\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\User\\AndroidStudioProjects\\ocv1\\openCV\\.cxx\\Debug\\j173p2e2\\x86" ^
+  opencv_jni_shared
